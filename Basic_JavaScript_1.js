797768fd14ffeee148c6291dfb79c698aea19379
TristanHillyard <73565670+TristanHillyard@users.noreply.github.com>
2020-11-01 17:12:38 -0700

    Create Basic_JavaScript_1.js
    New script.js

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+	<body>
+		<title>Basic JavaScript</title>
+		<meta charset="utf-8">
+	<body>
+	<main>
+		<script>
+			alert("Hello World! I am an alert box.")
+		</script>
+	</main>
+</html>
+	
