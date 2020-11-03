06a880a59dcc3fc75ac5801f074e071bc3740bcc
TristanHillyard <73565670+TristanHillyard@users.noreply.github.com>
2020-11-02 21:25:24 -0700

    Update Basic_JavaScript_1.js
    added new function

--- Basic_JavaScript_1.js
+++ Basic_JavaScript_1.js
@@ -2,12 +2,19 @@
 <html lang="en">
 	<body>
 		<title>Basic JavaScript</title>
-		<meta charset="utf-8">
-	<body>
+		<meta charset=utf-8">
+	</body>
 	<main>
 		<script>
-			alert("Hello World! I am an alert box.")
+			alert("Hello World, I am an alert box."
 		</script>
-	</main>
+
+		<script>
+			function My_First_Function() {
+				var String ="Kiss me, I'm Irish";
+				var result = String.fontcolor("green");
+			document.getElementById("Irish").innerHTML =
+			result;
+		}
+</script>
 </html>
-	
