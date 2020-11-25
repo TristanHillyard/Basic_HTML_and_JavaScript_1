072db2dd0c239415793df4353ffdaf3d641b5aaa
TristanHillyard <73565670+TristanHillyard@users.noreply.github.com>
2020-11-24 21:31:37 -0700

    updates
    yes

---
+++ Basic_HTML_and_CSS/One-Page Website/JS/one_page_website.js
@@ -0,0 +1,11 @@
+//opening the Modal
+function openModal() {
+    document.getElementById("gallery").style.display ="block";
+}
+//Closing the modal
+function closeModal(){
+    document.getElementById("galley").style.display = "none";
+}
+
+var slideIndex = 1;
+show
